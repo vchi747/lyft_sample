0f3fb3081ed69793dac33744025c162a5811e4d1
vchi747 <vchi747@gmail.com>
2021-06-14 01:39:28 -0700

    added content property checker

--- index.js
+++ index.js
@@ -9,11 +9,15 @@ app.use(Express.json());
 
 app.post("/test", (req,res) => {
     var string_to_manipulate = req.body.string_to_cut;
+    if (string_to_manipulate === undefined){
+        return response.status(400).json({error:'No Input Detected'})
+    }
     var index = 0;
     var out = '';
     //loop through the string to cut in order to get every third character
     while (string_to_manipulate[index]){
         if((index + 1) % 3 == 0){
+            //append it to the return string 
             out += string_to_manipulate[index];
             index += 1;
         }
